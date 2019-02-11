446bfde05a4b86bb0146fc6a4463d09368b4d06c
Jonas Faber <dev.jonas.faber@gmail.com>
2019-02-11 17:19:59 +0100

    rename

--- packages/protobuf-graphql-express/exampleServer.ts
+++ packages/protobuf-graphql-express/exampleServer.ts
@@ -5,7 +5,7 @@ import {
   createRoot,
   addReqMsgToNamespace,
   addQueryResMsgToNamespace,
-} from '../msg-pack-graphql-encoding/index';
+} from '../protobuf-graphql-encoding/index';
 import { execute, buildSchema, parse } from 'graphql';
 import { readFileSync } from 'fs';
 
@@ -16,6 +16,9 @@ const schema = buildSchema(
 app.use(
   bodyParser.raw({
     type: 'application/gqlproto',
+  }),
+  bodyParser.json({
+    type: 'application/json',
   })
 );
 app.all('/', function(req, res, next) {
@@ -23,7 +26,6 @@ app.all('/', function(req, res, next) {
     const { namespace: ns, root } = createRoot('graphql_server');
     addReqMsgToNamespace(ns);
     var RequestMessage = root.lookupType('graphql_server.Request');
-    console.log(req.body);
     const reqJSON = RequestMessage.decode(req.body).toJSON();
     const query = reqJSON.query;
     const data = cloneDeepWith(
@@ -43,15 +45,18 @@ app.all('/', function(req, res, next) {
 
     const resMsg = ResponseMessage.create(data);
     const bufferRes = ResponseMessage.encode(resMsg).finish();
-    if (req.header('json')) {
-      res.type('application/json');
-      res.send(JSON.stringify(data));
-    } else {
-      res.type('application/octet-stream');
-      res.send(Buffer.from(bufferRes));
-    }
-  } else {
-    res.send(req.headers);
+
+    res.type('application/octet-stream');
+    res.send(Buffer.from(bufferRes));
+  } else if (req.headers['content-type'] === 'application/json') {
+    const data = execute(schema, parse(req.body.query), {
+      test: {
+        string: 'foo',
+        number: 42,
+      },
+    });
+    res.type('application/json');
+    res.send(JSON.stringify(data));
   }
   next();
 });
